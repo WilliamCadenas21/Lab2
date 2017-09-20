8ad2e584c4ea3113c04246fe286c8e7cf2a4c7e9
Guest <Guest@10.10.42.30>
2017-09-20 10:44:05 -0500

    firts commit

---
+++ Laboratory2/src/laboratory2/Graph.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratory2;
+
+/**
+ *
+ * @author Guest
+ */
+public class Graph {
+    
+}
---
+++ Laboratory2/src/laboratory2/Vertex.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratory2;
+
+/**
+ *
+ * @author Guest
+ */
+public class Vertex {
+    String name;
+    
+    public String getNombre()   {
+        
+    }
+   
+            
+}
